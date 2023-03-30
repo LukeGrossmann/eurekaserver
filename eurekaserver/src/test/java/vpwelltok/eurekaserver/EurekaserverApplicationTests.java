7ba974c51a7f1192392f8829999bc7ec11607b23
Luke Grossmann <luke.grossmann@virginpulse.com>
2023-03-30 15:29:51 -0700

    basic setup for service discovery

---
+++ eurekaserver/src/test/java/vpwelltok/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package vpwelltok.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
